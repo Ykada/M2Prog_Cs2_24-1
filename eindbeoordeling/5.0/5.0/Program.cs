0dc6314fa768dc5fe81d7364cd96cb0ba0a4dcb1
Yk_ <bo.bolweg@gmail.com>
2025-01-23 12:56:19 +0100

    Update Program.cs

--- eindbeoordeling/5.0/5.0/Program.cs
+++ eindbeoordeling/5.0/5.0/Program.cs
@@ -24,5 +24,38 @@
                 Console.WriteLine(value); 
             }
         }
+        class Room
+        {
+            internal int x, y;
+            internal string[] level;
+
+            internal Room(int x, int y, string[] level)
+            {
+                this.x = x;
+                this.y = y;
+                this.level = level;
+            }
+            Room start = new Room(0, 0, [
+                "XXXXX",
+                "X   X",
+                "X   X",
+                "X   X",
+                "XXXXX"
+            ]);
+        }
+        class Rooms
+        {
+            //Rooms.Add(start.getroomlocationkey(), start);
+        }
+        private void ShowRooms(int x, int y)
+        {
+            Room room = Rooms[$"{x}, {y}"];
+
+            Console.WriteLine($" you are in room {x}, {y}");
+            foreach (string r in room.level)
+            {
+                Console.WriteLine(r);
+            }
+        }
     }
 }
